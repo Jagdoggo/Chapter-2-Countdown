a78a154b9d4c2c462245c7a11daeb2a87e9ae1c5
Jagdoggo <emlynharrison2000@gmail.com>
2026-07-16 13:54:27 +1200

    Added trailer 2 mode

--- menu.gd
+++ menu.gd
@@ -1,8 +1,10 @@
 extends Control
 
 @onready var sooner: CheckButton = $VBoxContainer/Sooner
+@onready var until_trailer_2: CheckButton = $"VBoxContainer/Until Trailer #2"
 
 var last_days : int = -INF
+var extra = load("res://extra_message.tscn")
 
 func _process(delta: float) -> void:
 	var string : String
@@ -14,8 +16,10 @@ func _process(delta: float) -> void:
 		"minute": 0,
 		"second": 0
 	}
+	if until_trailer_2.button_pressed:
+		target["day"] = 17
 	if sooner.button_pressed:
-		target["day"] = 22
+		target["day"] -= 2
 	var target_unix = Time.get_unix_time_from_datetime_dict(target)
 	var current_unix = Time.get_unix_time_from_system()
 	var difference = target_unix - current_unix
@@ -28,17 +32,23 @@ func _process(delta: float) -> void:
 	hours = int(difference / 3600) % 24
 	days = int(difference / 86400)
 	if difference <= 0:
-		string = "GO PLAY RIGHT NOW"
+		if until_trailer_2.button_pressed:
+			string = "GO WATCH RIGHT NOW"
+		else:
+			string = "GO PLAY RIGHT NOW"
 	else:
 		string = "%d days, %02d hours, %02d minutes, and %02d seconds" % [days, hours, minutes, seconds]
 	$VBoxContainer/Time.text = "Chapter 2 in: " + string
 	if days != last_days:
 		if days >= 0:
 			var ammount = round(2500/pow(days+0.75,4.36))
-			if ammount > 240:
-				ammount = 240
+			if ammount > 216:
+				ammount = 216
 			for child in $VBoxContainer/BoxContainer.get_children():
 				child.queue_free()
 			for i in range(ammount):
-				$VBoxContainer/BoxContainer.add_child(load("res://extra_message.tscn").instantiate())
+				var node = extra.instantiate()
+				if until_trailer_2.button_pressed:
+					node.text = "Trailer 2"
+				$VBoxContainer/BoxContainer.add_child(node)
 	last_days = days
